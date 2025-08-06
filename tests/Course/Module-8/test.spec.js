fea1b2bafa6a3198029ac864bd8306ef762b74d8
unknown <bipin.kumar.pros@gmail.com>
2025-08-06 13:51:09 +0530

    test results

--- tests/Course/Module-8/test.spec.js
+++ tests/Course/Module-8/test.spec.js
@@ -1,11 +1,10 @@
 import {test,expect} from "@playwright/test"
 
 test("screenShots", async ({ page }) => {
-    await page.goto("https://www.google.com");
-    const img = await page.locator(".LS8OJ").screenshot()
-    expect(img).toMatchSnapshot('google.png');
+  await page.goto("https://www.google.com");
+  const img = await page.locator(".LS8OJ").screenshot();
+  expect(img).toMatchSnapshot('googl-logo-win32.png'); 
 });
-
 
 
 // test('should download a file and save it', async ({ browser }) => {
