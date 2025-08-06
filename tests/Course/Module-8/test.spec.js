33e8eea33a1cffb2da4d7420fef0ce473a699df3
unknown <bipin.kumar.pros@gmail.com>
2025-08-06 14:00:17 +0530

    test results

--- tests/Course/Module-8/test.spec.js
+++ tests/Course/Module-8/test.spec.js
@@ -3,7 +3,7 @@ import {test,expect} from "@playwright/test"
 test("screenShots", async ({ page }) => {
   await page.goto("https://www.google.com");
   const img = await page.locator(".LS8OJ").screenshot();
-  expect(img).toMatchSnapshot('googl-logo-win32.png'); 
+  expect(img).toMatchSnapshot('google-logo-win32.png'); 
 });
 
 
